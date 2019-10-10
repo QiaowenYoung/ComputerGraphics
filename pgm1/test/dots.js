7909e9f4c4e0a191f39a6fc287d576906b19d1ff
QiaowenYoung <17307130294@fudan.edu.cn>
2019-10-09 23:28:13 -0700

    10/09/19

--- pgm1/test/dots.js
+++ pgm1/test/dots.js
@@ -48,8 +48,11 @@ function main() {
 
     // Specify the color for clearing <canvas>
     gl.clearColor(1, 1, 1, 1);
-}
 
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
+var c = 0;
 function click(ev, gl, canvas) {
     var x = ev.clientX; // x coordinate of a mouse pointer
     var y = ev.clientY; // y coordinate of a mouse pointer
@@ -66,6 +69,7 @@ function click(ev, gl, canvas) {
             colors.push(1.0);
             colors.push(0.0);
             colors.push(0.0);
+            colors.push(1.0);
         }
     }
     else if(ev.button === 2){
@@ -75,6 +79,7 @@ function click(ev, gl, canvas) {
         for(var i = 0; i < len; i++){
             colors.push(0.0);
             colors.push(0.0);
+            colors.push(1.0);
             colors.push(1.0);
         }
     }
@@ -118,7 +123,7 @@ function click(ev, gl, canvas) {
         return -1;
     }
     //gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
-    gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(a_Color, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(a_Color);
 
     // Unbind the buffer object
@@ -133,7 +138,7 @@ function click(ev, gl, canvas) {
 
     // Set the matrix to be used for to set the camera view
     var viewMatrix = new Matrix4();
-    viewMatrix.setLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+    viewMatrix.setLookAt(0, 0, 75, 0, 0, 0, -1, 0, 0);
 
     // Set the view matrix
     gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
@@ -154,8 +159,8 @@ function click(ev, gl, canvas) {
  * an array that stores all the coordinates to be passed to buffer
  */
 function trees(x, y, n){
-    var str1 = generate_expression(2, "");
-    var str2 = generate_expression(2, "");
+    var str1 = generate_expression(4, "");
+    var str2 = generate_expression(6, "");
     if(n === 1){
         return generate_tree(x, y, str1, 50);
     }
@@ -181,7 +186,7 @@ function generate_tree(x, y, str, l){
     for(var i = 0; i < len;){
         if(str[i] === '1'){
             var j;
-            stack.push([x, y, 0]);
+            stack.push([x-Math.sqrt(2)*l, y, -Math.sqrt(2)*l]);
             stack.push([x, y, 0]);
             stack.push([x, y, l]);
             for(j = i; j < len && str[j] === '1'; j++);
@@ -279,6 +284,7 @@ function generate_tree(x, y, str, l){
  * an array of the coordinate of next point to be generated
  */
 function generate_point(x0, y0, z0, x1, y1, z1, x2, y2, z2, alpha, beta){
+    c++;
     var v0 = [x1-x0, y1-y0, z1-z0]; // father vector
     var v1 = [x2-x1, y2-y1, z2-z1]; // current vector
     var nx = [];
@@ -286,35 +292,47 @@ function generate_point(x0, y0, z0, x1, y1, z1, x2, y2, z2, alpha, beta){
     var nz = [];
 
     // calculate the (nx, ny, nz) for the current space
-    var l0 = Math.sqrt((x1-x0)^2+(y1-y0)^2+(z1-z0)^2);
-    var l1 = Math.sqrt((x2-x1)^2+(y2-y1)^2+(z2-z1)^2);
+    var l0 = Math.sqrt(Math.pow((x1-x0),2)+Math.pow((y1-y0),2)+Math.pow((z1-z0),2));
+    var l1 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)+Math.pow((z2-z1),2));
     // standardize v0 and v1
-    v0 /= l0;
-    v1 /= l1;
-    nz = v1;
-    nx = Math.sqrt(2)*v0-v1;
+    v0[0] = v0[0]/l0;
+    v0[1] = v0[1]/l0;
+    v0[2] = v0[2]/l0;
+    v1[0] = v1[0]/l1;
+    v1[1] = v1[1]/l1;
+    v1[2] = v1[2]/l1;
+
+    nz.push(v1[0]);
+    nz.push(v1[1]);
+    nz.push(v1[2]);
+    nx.push(Math.sqrt(2)*v0[0]-v1[0]);
+    nx.push(Math.sqrt(2)*v0[1]-v1[1]);
+    nx.push(Math.sqrt(2)*v0[2]-v1[2]);
     ny.push(nz[1]*nx[2]-nx[2]*nx[1]);
     ny.push(nz[2]*nx[0]-nx[2]*nz[0]);
     ny.push(nz[0]*nx[1]-nx[0]*nz[1]);
     
     // rotate by y'_axis for beta, then by z'_axis for alpha
-    var u, t, v;
-    u = 0; t = 0; v = 1;
-    u = u*Math.sin(beta)+v*Math.cos(beta);
-    t = 0;
-    v = u*Math.cos(beta)-v*Math.sin(beta);
+    var u0, t0, v0, u, t, v;
+    u0 = 0; t0 = 0; v0 = 1;
+    u = v0*Math.sin(beta)+u0*Math.cos(beta);
+    t = t0;
+    v = v0*Math.cos(beta)-u0*Math.sin(beta);
 
-    u = u*Math.cos(alpha)-t*Math.sin(alpha);
-    t = u*Math.sin(alpha)+t*Math.cos(alpha);
-    v = v;
+    u0 = u; t0 = t; v0 = v;
+    u = u0*Math.cos(alpha)-t0*Math.sin(alpha);
+    t = u0*Math.sin(alpha)+t0*Math.cos(alpha);
+    v = v0;
     
     // change base vectors from [nx, ny, nz] to [X, Y, Z]
-    var vec;
-    vec = u*nx+t*ny+v*nz;
-    var l = Math.sqrt(u^2+t^2+v^2);
-    vec /= l;
-    vec *= l1/2;
-    vec += [x2, y2, z2];
+    var x = u*nx[0]+t*ny[0]+v*nz[0];
+    var y = u*nx[1]+t*ny[1]+v*nz[1];
+    var z = u*nx[2]+t*ny[2]+v*nz[2];
+    x = x*(l1/2)+x2;
+    y = y*(l1/2)+y2;
+    z = z*(l1/2)+z2;
+    var vec = [x, y, z];
+    console.log(`vec[${c}]: ${vec}`);
     return vec;
 }
 
