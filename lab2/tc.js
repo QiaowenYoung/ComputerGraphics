7d594393ef59c9ece04c869b66bd7220a179fc21
QiaowenYoung <17307130294@fudan.edu.cn>
2019-10-17 16:29:10 -0700

    10/17/19

--- lab2/tc.js
+++ lab2/tc.js
@@ -1,3 +1,4 @@
+/*
 // Vertex shader program
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
@@ -23,9 +24,41 @@ var FSHADER_SOURCE =
     '  vec3 diffuse = u_color.rgb * vec3(1.0, 0.0, 0.0) * light;\n' +
     '  gl_FragColor = vec4(diffuse, 1.0);\n' +
     '}\n';
+*/
+
+// Vertex shader program
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    //'attribute vec4 a_Color;\n' + 
+    'attribute vec3 a_Normal;\n' +        // Normal
+    'uniform mat4 u_MvpMatrix;\n' +
+    'uniform vec3 u_LightColor;\n' +     // Light color
+    'uniform vec3 u_LightDirection;\n' + // Light direction (in the world coordinate, normalized)
+    'varying vec4 v_Color;\n' +
+    'void main() {\n' +
+    '  gl_Position = u_MvpMatrix * a_Position ;\n' +
+    // Make the length of the normal 1.0
+    '  vec3 normal = normalize(a_Normal);\n' +
+    // Dot product of the light direction and the orientation of a surface (the normal)
+    '  float nDotL = max(dot(u_LightDirection, normal), 0.0);\n' +
+    // Calculate the color due to diffuse reflection
+    '  vec3 diffuse = u_LightColor * vec3(1.0, 0.0, 0.0) * nDotL;\n' +
+    '  v_Color = vec4(diffuse, 1.0);\n' +
+    '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE =
+    '#ifdef GL_ES\n' +
+    'precision mediump float;\n' +
+    '#endif\n' +
+    'varying vec4 v_Color;\n' +
+    'void main() {\n' +
+    '  gl_FragColor = v_Color;\n' +
+    '}\n';
 
 var points = [];
 var n = [];
+var toggle1 = 0, toggle2 = 0, toggle3 = 0;
 function main() {
     // Retrieve <canvas> element
     var canvas = document.getElementById('webgl');
@@ -68,21 +101,6 @@ function main() {
     gl.enableVertexAttribArray(a_Position);
     /* end */
 
-    /* decide u_MvpMatrix */
-    // Get the storage location of u_MvpMatrix
-    var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
-    if (!u_MvpMatrix) {
-        console.log('Failed to get the storage location of u_MvpMatrix');
-        return;
-    }
-    // Set the eye point and the viewing volume
-    var mvpMatrix = new Matrix4();
-    mvpMatrix.setPerspective(150, canvas.width / canvas.height, 1, 100);
-    mvpMatrix.lookAt(5, 0, 0, 0, 0, 0, 0, 0, 1);
-    // Pass the model view projection matrix to u_MvpMatrix
-    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
-    /* end*/
-
     /* decide normals */
     setNormals();
     var normals = new Float32Array(n);
@@ -91,7 +109,7 @@ function main() {
     // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
     gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-    var normalLocation = gl.getAttribLocation(gl.program, "a_normal");
+    var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
     gl.vertexAttribPointer(normalLocation, 3, gl.FLOAT, false, 0, 0);
     // Turn on the normal attribute
     gl.enableVertexAttribArray(normalLocation);
@@ -99,17 +117,17 @@ function main() {
     /* end */
 
     /* decide light color */
-    var colorLocation = gl.getUniformLocation(gl.program, "u_color");
+    var colorLocation = gl.getUniformLocation(gl.program, "u_LightColor");
     // Set the color to use
-    gl.uniform4fv(colorLocation, [1.0, 1.0, 1.0, 1.0]); // white
+    gl.uniform3fv(colorLocation, [1.0, 1.0, 1.0]); // white
     /* end */
 
     /* decide direction of light */
-    var reverseLightDirectionLocation =
-        gl.getUniformLocation(gl.program, "u_reverseLightDirection");
+    var LightDirectionLocation =
+        gl.getUniformLocation(gl.program, "u_LightDirection");
     var lightDirection = new Vector3([1.0, 1.0, 1.0]);
     lightDirection.normalize();     // Normalize
-    gl.uniform3fv(reverseLightDirectionLocation, lightDirection.elements);
+    gl.uniform3fv(LightDirectionLocation, lightDirection.elements);
     /* end */
 
     // Specify the color for clearing <canvas>
@@ -118,8 +136,122 @@ function main() {
     // Clear color and depth buffer
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+    /* decide default view direction */
+    // Get the storage location of u_MvpMatrix
+    var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+    if (!u_MvpMatrix) {
+        console.log('Failed to get the storage location of u_MvpMatrix');
+        return;
+    }
+    // Set the eye point and the viewing volume
+    var mvpMatrix = new Matrix4();
+    //mvpMatrix.setPerspective(150, canvas.width / canvas.height, 1, 100);
+    mvpMatrix.setOrtho(-1, 1, -1, 1, -2, 2);
+    mvpMatrix.setLookAt(0, 0, 1, 0, 0, 0, 0, 1, 0);
+    // Pass the model view projection matrix to u_MvpMatrix
+    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+    /* end */
+
     var len = points.length / 3;
     gl.drawArrays(gl.TRIANGLES, 0, len);
+
+    /* decide u_MvpMatrix */
+    var checkbox1 = document.getElementById('toggle1');
+    checkbox1.addEventListener('change', function () {
+        if (checkbox1.checked) {
+            toggle1 = 1;
+            setSideView(gl);
+        } else {
+            toggle1 = 0;
+            setTopView(gl);
+        }
+    });
+
+    var checkbox2 = document.getElementById('toggle2');
+    checkbox2.addEventListener('change', function () {
+        if (checkbox2.checked) {
+            toggle2 = 1;
+            if (toggle1 == 0) {
+                setTopView(gl);
+            }
+            else {
+                setSideView(gl);
+            }
+        } else {
+            toggle2 = 0;
+            if (toggle1 == 0) {
+                setTopView(gl);
+                if (toggle1 == 0) {
+                    setTopView(gl);
+                }
+                else {
+                    setSideView(gl);
+                }
+            }
+            else {
+                setSideView(gl);
+                if (toggle1 == 0) {
+                    setTopView(gl);
+                }
+                else {
+                    setSideView(gl);
+                }
+            }
+        }
+    });
+
+    var checkbox3 = document.getElementById('toggle3');
+    checkbox3.addEventListener('change', function () {
+        if (checkbox3.checked) {
+            toggle3 = 1;
+        } else {
+            toggle3 = 0;
+        }
+    });
+}
+
+function setTopView(gl) {
+    // Get the storage location of u_MvpMatrix
+    var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+    if (!u_MvpMatrix) {
+        console.log('Failed to get the storage location of u_MvpMatrix');
+        return;
+    }
+    // Set the eye point and the viewing volume
+    var mvpMatrix = new Matrix4();
+    mvpMatrix.setOrtho(-1, 1, -1, 1, -2, 2);
+    mvpMatrix.setLookAt(0, 0, 1, 0, 0, 0, 0, 1, 0);
+    // Pass the model view projection matrix to u_MvpMatrix
+    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+    var len = points.length / 3;
+    if (toggle2 == 0 && toggle3 == 0) {
+        gl.drawArrays(gl.TRIANGLES, 0, len);
+    }
+    else if (toggle2 == 1 && toggle3 == 0) {
+        gl.drawArrays(gl.LINE_STRIP, 0, len);
+    }
+}
+
+function setSideView(gl) {
+    // Get the storage location of u_MvpMatrix
+    var u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+    if (!u_MvpMatrix) {
+        console.log('Failed to get the storage location of u_MvpMatrix');
+        return;
+    }
+    // Set the eye point and the viewing volume
+    var mvpMatrix = new Matrix4();
+    mvpMatrix.setOrtho(-1, 1, -1, 1, -2, 2);
+    mvpMatrix.setLookAt(0, -1, 0.75, 0, 0, 0, 0, 1, 0);
+    // Pass the model view projection matrix to u_MvpMatrix
+    gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+    var len = points.length / 3;
+    if (toggle2 == 0 && toggle3 == 0) {
+        gl.drawArrays(gl.TRIANGLES, 0, len);
+    }
+    else if (toggle2 == 1 && toggle3 == 0) {
+        gl.drawArrays(gl.LINE_STRIP, 0, len);
+    }
 }
 
 /* setNormals:
@@ -133,18 +265,46 @@ function main() {
 function setNormals() {
     var len = points.length;
     for (var i = 0; i < len; i += 9) {
-        var a = [points[0], points[1], points[2]];
-        var b = [points[3], points[4], points[5]];
-        var c = [points[6], points[7], points[8]];
+        var a = [points[i], points[i + 1], points[i + 2]];
+        var b = [points[i + 3], points[i + 4], points[i + 5]];
+        var c = [points[i + 6], points[i + 7], points[i + 8]];
+        var ac = [c[0] - a[0], c[1] - a[1], c[2] - a[2]];
+        var ab = [b[0] - a[0], b[1] - a[1], b[2] - a[2]];
         var ba = [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
         var bc = [c[0] - b[0], c[1] - b[1], c[2] - b[2]];
-        var normal = [];
-        normal.push(bc[1] * ba[2] - bc[2] * ba[1]);
-        normal.push(ba[0] * bc[2] - ba[2] * bc[0]);
-        normal.push(bc[0] * ba[1] - bc[1] * ba[0]);
-        n = n.concat(normal);
-        n = n.concat(normal);
-        n = n.concat(normal);
+        var ca = [a[0] - c[0], a[1] - c[1], a[2] - c[2]];
+        var cb = [b[0] - c[0], b[1] - c[1], b[2] - c[2]];
+        var normala = [];
+        var normalb = [];
+        var normalc = [];
+
+        normala.push(ab[1] * ac[2] - ab[2] * ac[1]);
+        normala.push(ac[0] * ab[2] - ac[2] * ab[0]);
+        normala.push(ab[0] * ac[1] - ab[1] * ac[0]);
+        var lena = Math.sqrt(Math.pow(normala[0], 2) + Math.pow(normala[1], 2) + Math.pow(normala[2], 2));
+        normala[0] /= lena;
+        normala[1] /= lena;
+        normala[2] /= lena;
+
+        normalb.push(bc[1] * ba[2] - bc[2] * ba[1]);
+        normalb.push(ba[0] * bc[2] - ba[2] * bc[0]);
+        normalb.push(bc[0] * ba[1] - bc[1] * ba[0]);
+        var lenb = Math.sqrt(Math.pow(normalb[0], 2) + Math.pow(normalb[1], 2) + Math.pow(normalb[2], 2));
+        normalb[0] /= lenb;
+        normalb[1] /= lenb;
+        normalb[2] /= lenb;
+
+        normalc.push(ca[1] * cb[2] - ca[2] * cb[1]);
+        normalc.push(cb[0] * ca[2] - cb[2] * ca[0]);
+        normalc.push(ca[0] * cb[1] - ca[1] * cb[0]);
+        var lenc = Math.sqrt(Math.pow(normalc[0], 2) + Math.pow(normalc[1], 2) + Math.pow(normalc[2], 2));
+        normalc[0] /= lenc;
+        normalc[1] /= lenc;
+        normalc[2] /= lenc;
+
+        n = n.concat(normala);
+        n = n.concat(normalb);
+        n = n.concat(normalc);
     }
 }
 
@@ -158,10 +318,10 @@ function setNormals() {
  * this cylindar is located at (0, 0)
  */
 function generate_tc() {
-    var r1 = 0.5;
-    var r2 = 1;
+    var r1 = 0.05;
+    var r2 = 0.1;
     var theta = Math.PI / 6;
-    var height = 10;
+    var height = 1;
     var topx = [], topy = [];
     var bottomx = [], bottomy = [];
     for (var i = 0; i < 13; i++) {
@@ -195,32 +355,3 @@ function generate_tc() {
         points.push(0.0);
     }
 }
-
-/* generate_color:
- * input:
- * (x0, y0, z0), (x1, y1, z1), (x2, y2, z2): points of a triangle in CCW
- * output:
- * an array that stores the rgb color of the plane constructed by these three points
- *//*
-function generate_color(x0, y0, z0, x1, y1, z1, x2, y2, z2) {
-var color = [];
-var ba = [x0 - x1, y0 - y1, z0 - z1];
-var bc = [x2 - x1, y2 - y1, z2 - z1];
-// Do cross product of bc and ba
-var normal = [];
-normal.push(bc[1] * ba[2] - bc[2] * ba[1]);
-normal.push(ba[0] * bc[2] - ba[2] * bc[0]);
-normal.push(bc[0] * ba[1] - bc[1] * ba[0]);
-
-// Calculate cos(theta): dot product of normalized normal and light
-var light = [1.0, 1.0, 1.0];
-var len1 = Math.sqrt(Math.pow(normal[0], 2) + Math.pow(normal[1], 2) + Math.pow(normal[2], 2));
-var len2 = Math.sqrt(1 + 1 + 1);
-normal[0] = normal[0] / len1;
-normal[1] = normal[1] / len1;
-normal[2] = normal[2] / len1;
-light[0] = light[0] / len2;
-light[1] = light[1] / len2;
-light[2] = light[2] / len2;
-var costheta = normal[0] * light[0] + normal[1] * light[1] + normal[2] * light[2];
-}*/
