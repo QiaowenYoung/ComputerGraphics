828d1cd1a20e545738f5e2580f6c23c8c87c2e74
QiaowenYoung <17307130294@fudan.edu.cn>
2019-10-25 21:24:43 -0700

    10/25/19

--- prog2/flesh.js
+++ prog2/flesh.js
@@ -114,12 +114,39 @@ function main() {
     });
 }
 
-function save(){
-
+// Save: refer to https://jsfiddle.net/4v26ebtp/
+function fakeClick(obj) {
+    var ev = document.createEvent("MouseEvents");
+    ev.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    obj.dispatchEvent(ev);
 }
 
-function load(){
-    
+function exportRaw(name, data) {
+    var urlObject = window.URL || window.webkitURL || window;
+    var export_blob = new Blob([data]);
+    var save_link = document.createElementNS("http://www.w3.org/1999/xhtml", "a")
+    save_link.href = urlObject.createObjectURL(export_blob);
+    save_link.download = name;
+    fakeClick(save_link);
+}
+
+function save() {
+    var arr = [];
+    arr.push(toggle1);
+    arr.push(toggle2);
+    arr.push(toggle3);
+    arr.push(countl);
+    arr = arr.concat(Txl);
+    arr = arr.concat(Tyl);
+    arr.push(countr);
+    arr = arr.concat(Txr);
+    arr = arr.concat(Tyr);
+    exportRaw('test.txt', JSON.stringify(arr));
+}
+
+
+function load() {
+
 }
 
 function setView(gl, cylinderProgram) {
