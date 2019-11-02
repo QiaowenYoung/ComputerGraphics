b534ecf2d57190bf0151c93b1b6109aa77f895d0
QiaowenYoung <17307130294@fudan.edu.cn>
2019-11-01 19:40:40 -0700

    11/1/19

--- lab3/gouraud.js
+++ lab3/gouraud.js
@@ -10,9 +10,7 @@ var VSHADER_SOURCE =
     'varying vec4 v_Color;\n' +
     'varying vec3 v_vertPos;\n' + // View Direction
     'uniform float u_shininessVal;\n' + // 20 or 5
-    //'uniform mat4 u_modelview;\n' +
     'void main() {\n' +
-    //'  vec4 v_vertPos4 = u_modelview * a_Position;\n' +
     '  vec4 v_vertPos4 = u_mvpMatrix * a_Position;\n' +
     '  v_vertPos = vec3(v_vertPos4) / v_vertPos4.w;\n' +
     '  gl_Position = u_mvpMatrix * a_Position + u_Translation;\n' +
@@ -30,7 +28,7 @@ var VSHADER_SOURCE =
     '    float specAngle = max(dot(R, V), 0.0);\n' +
     '    specular = pow(specAngle, u_shininessVal);\n' +
     '  }\n' +
-    '  v_Color = vec4(diffuse + vec3(1.0, 1.0, 1.0) * specular * vec3(1.0, 1.0, 1.0), a_Color.a);\n' +
+    '  v_Color = vec4(diffuse + vec3(1.0, 1.0, 1.0) * vec3(1.0, 1.0, 1.0) * specular, a_Color.a);\n' +
     '}\n';
 
 // Fragment shader program
@@ -51,7 +49,6 @@ var countr = 0; // # of blue trees
 // toggle2: 0 for ortho view, 1 for pers view
 // toggle3: 0 for solid, 1 for wireframe
 var toggle1 = 0, toggle2 = 0, toggle3 = 0;
-//var modelview = new Float32Array(16);
 function main() {
     // Retrieve <canvas> element
     var canvas = document.getElementById('webgl');
@@ -122,16 +119,22 @@ function main() {
             setView(gl, cylinderProgram);
         }
     });
+    
+    toggle3 = 0;
+    setView(gl, cylinderProgram);
 
-    var checkbox3 = document.getElementById('toggle3');
-    checkbox3.addEventListener('change', function () {
-        if (checkbox3.checked) {
-            toggle3 = 1;
-            setView(gl, cylinderProgram);
-        } else {
-            toggle3 = 0;
-            setView(gl, cylinderProgram);
-        }
+    var rad = document.form.tree;
+    rad[0].addEventListener('change', function () {
+        toggle3 = 0;
+        setView(gl, cylinderProgram);
+    });
+    rad[1].addEventListener('change', function () {
+        toggle3 = 1;
+        setView(gl, cylinderProgram);
+    });
+    rad[2].addEventListener('change', function () {
+        toggle3 = 2;
+        setView(gl, cylinderProgram);
     });
 
     var submit = document.getElementById('submit');
@@ -251,7 +254,6 @@ function setView(gl, cylinderProgram) {
     for (var i = 0; i < countl; i++) { // Draw red trees
         initPositions(gl, cylinderProgram, 0);
         initColors(gl, cylinderProgram, 0);
-        initNormals(gl, cylinderProgram, 0);
         initLightColor(gl, cylinderProgram);
         initLightDirection(gl, cylinderProgram);
         initMatrix(gl, cylinderProgram, toggle1, toggle2);
@@ -259,26 +261,37 @@ function setView(gl, cylinderProgram) {
         initGloss(gl, cylinderProgram, 0);
         var len = cylinderl.length / 3;
         if (toggle3 == 0) { // Flat shading
+            initNormals(gl, cylinderProgram, 0, 0);
             gl.drawArrays(gl.TRIANGLES, 0, len);
         }
-        else { // wireframe
+        else if (toggle3 == 1) { // smooth
+            initNormals(gl, cylinderProgram, 0, 1);
+            gl.drawArrays(gl.TRIANGLES, 0, len);
+        }
+        else if (toggle3 == 2) { // wireframe using flat shading normals
+            initNormals(gl, cylinderProgram, 0, 0);
             gl.drawArrays(gl.LINES, 0, len);
         }
     }
     for (var i = 0; i < countr; i++) { // Draw blue trees
         initPositions(gl, cylinderProgram, 1);
         initColors(gl, cylinderProgram, 1);
-        initNormals(gl, cylinderProgram, 1);
         initLightColor(gl, cylinderProgram);
         initLightDirection(gl, cylinderProgram);
         initMatrix(gl, cylinderProgram, toggle1, toggle2);
         initTranslation(gl, cylinderProgram, Txr[i], Tyr[i], toggle2);
         initGloss(gl, cylinderProgram, 1);
         var len = cylinderr.length / 3;
-        if (toggle3 == 0) {
+        if (toggle3 == 0) { // Flat shading
+            initNormals(gl, cylinderProgram, 1, 0);
             gl.drawArrays(gl.TRIANGLES, 0, len);
         }
-        else {
+        else if (toggle3 == 1) { // smooth
+            initNormals(gl, cylinderProgram, 1, 1);
+            gl.drawArrays(gl.TRIANGLES, 0, len);
+        }
+        else if (toggle3 == 2) { // wireframe using flat shading normals
+            initNormals(gl, cylinderProgram, 1, 0);
             gl.drawArrays(gl.LINES, 0, len);
         }
     }
@@ -366,45 +379,82 @@ function initColors(gl, cylinderProgram, tag) {
 /* initNormals
  * input:
  * gl, cylinderProgram
- * tag: 0 for red tree, 1 for blue tree
+ * tag1: 0 for red tree, 1 for blue tree
+ * tag2: 0 for flat shading normals, 1 for smooth shading normals
  * output:
  * none
  * use:
  * initialize a_Normal
  */
-function initNormals(gl, cylinderProgram, tag) {
+function initNormals(gl, cylinderProgram, tag1, tag2) {
     gl.useProgram(cylinderProgram);
-    if (tag == 0) {
-        var normals = new Float32Array(nl);
-        // Create a buffer to put normals in
-        var normalBuffer = gl.createBuffer();
-        if (!normalBuffer) {
-            console.log('Failed to create the buffer object');
-            return -1;
+    if (tag1 == 0) {
+        if (tag2 == 0) {
+            var normals = new Float32Array(nnl);
+            // Create a buffer to put normals in
+            var normalBuffer = gl.createBuffer();
+            if (!normalBuffer) {
+                console.log('Failed to create the buffer object');
+                return -1;
+            }
+            // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
+            gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
+            //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
+            gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
+            // Turn on the normal attribute
+            gl.enableVertexAttribArray(cylinderProgram.a_Normal);
         }
-        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
-        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-        //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
-        gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
-        // Turn on the normal attribute
-        gl.enableVertexAttribArray(cylinderProgram.a_Normal);
+        else {
+            var normals = new Float32Array(nl);
+            // Create a buffer to put normals in
+            var normalBuffer = gl.createBuffer();
+            if (!normalBuffer) {
+                console.log('Failed to create the buffer object');
+                return -1;
+            }
+            // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
+            gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
+            //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
+            gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
+            // Turn on the normal attribute
+            gl.enableVertexAttribArray(cylinderProgram.a_Normal);
+        }
     }
     else {
-        var normals = new Float32Array(nr);
-        // Create a buffer to put normals in
-        var normalBuffer = gl.createBuffer();
-        if (!normalBuffer) {
-            console.log('Failed to create the buffer object');
-            return -1;
+        if (tag2 == 0) {
+            var normals = new Float32Array(nnr);
+            // Create a buffer to put normals in
+            var normalBuffer = gl.createBuffer();
+            if (!normalBuffer) {
+                console.log('Failed to create the buffer object');
+                return -1;
+            }
+            // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
+            gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
+            //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
+            gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
+            // Turn on the normal attribute
+            gl.enableVertexAttribArray(cylinderProgram.a_Normal);
         }
-        // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
-        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-        //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
-        gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
-        // Turn on the normal attribute
-        gl.enableVertexAttribArray(cylinderProgram.a_Normal);
+        else {
+            var normals = new Float32Array(nr);
+            // Create a buffer to put normals in
+            var normalBuffer = gl.createBuffer();
+            if (!normalBuffer) {
+                console.log('Failed to create the buffer object');
+                return -1;
+            }
+            // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
+            gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+            gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
+            //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
+            gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
+            // Turn on the normal attribute
+            gl.enableVertexAttribArray(cylinderProgram.a_Normal);
+        }
     }
 }
 
@@ -512,60 +562,4 @@ function initGloss(gl, cylinderProgram, tag) {
     else {
         gl.uniform1f(cylinderProgram.u_shininessVal, 20.0);
     }
-}
-
-function mat4LookAt(viewMatrix,
-    eyeX, eyeY, eyeZ,
-    centerX, centerY, centerZ,
-    upX, upY, upZ) {
-
-    var dir = new Float32Array(3);
-    var right = new Float32Array(3);
-    var up = new Float32Array(3);
-    var eye = new Float32Array(3);
-
-    up[0] = upX; up[1] = upY; up[2] = upZ;
-    eye[0] = eyeX; eye[1] = eyeY; eye[2] = eyeZ;
-
-    dir[0] = centerX - eyeX; dir[1] = centerY - eyeY; dir[2] = centerZ - eyeZ;
-    vec3Normalize(dir);
-    vec3Cross(dir, up, right);
-    vec3Normalize(right);
-    vec3Cross(right, dir, up);
-    vec3Normalize(up);
-    // first row
-    viewMatrix[0] = right[0];
-    viewMatrix[4] = right[1];
-    viewMatrix[8] = right[2];
-    viewMatrix[12] = -vec3Dot(right, eye);
-    // second row
-    viewMatrix[1] = up[0];
-    viewMatrix[5] = up[1];
-    viewMatrix[9] = up[2];
-    viewMatrix[13] = -vec3Dot(up, eye);
-    // third row
-    viewMatrix[2] = -dir[0];
-    viewMatrix[6] = -dir[1];
-    viewMatrix[10] = -dir[2];
-    viewMatrix[14] = vec3Dot(dir, eye);
-    // forth row
-    viewMatrix[3] = 0.0;
-    viewMatrix[7] = 0.0;
-    viewMatrix[11] = 0.0;
-    viewMatrix[15] = 1.0;
-}
-
-function vec3Dot(a, b) {
-    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-
-function vec3Cross(a, b, res) {
-    res[0] = a[1] * b[2] - b[1] * a[2];
-    res[1] = a[2] * b[0] - b[2] * a[0];
-    res[2] = a[0] * b[1] - b[0] * a[1];
-}
-
-function vec3Normalize(a) {
-    var mag = Math.sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
-    a[0] /= mag; a[1] /= mag; a[2] /= mag;
 }
