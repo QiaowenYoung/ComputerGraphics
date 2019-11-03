5e8e781db5e1d4912a922d42f7611cb2516ecc4d
QiaowenYoung <17307130294@fudan.edu.cn>
2019-11-02 22:38:02 -0700

    11/2/19

--- lab3/gouraud.js
+++ lab3/gouraud.js
@@ -2,6 +2,7 @@
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
     'attribute vec4 a_Color;\n' +
+    'attribute vec4 a_Color_2;\n' +
     'attribute vec3 a_Normal;\n' +        // Normal
     'uniform mat4 u_mvpMatrix;\n' +
     'uniform vec4 u_Translation;\n' +
@@ -10,6 +11,8 @@ var VSHADER_SOURCE =
     'varying vec4 v_Color;\n' +
     'varying vec3 v_vertPos;\n' + // View Direction
     'uniform float u_shininessVal;\n' + // 20 or 5
+    'uniform bool u_Clicked;\n' + // Mouse is pressed
+    'uniform vec3 u_Ks;\n' + // Ks
     'void main() {\n' +
     '  vec4 v_vertPos4 = u_mvpMatrix * a_Position;\n' +
     '  v_vertPos = vec3(v_vertPos4) / v_vertPos4.w;\n' +
@@ -28,7 +31,10 @@ var VSHADER_SOURCE =
     '    float specAngle = max(dot(R, V), 0.0);\n' +
     '    specular = pow(specAngle, u_shininessVal);\n' +
     '  }\n' +
-    '  v_Color = vec4(diffuse + vec3(1.0, 1.0, 1.0) * vec3(1.0, 1.0, 1.0) * specular, a_Color.a);\n' +
+    '  v_Color = vec4(diffuse + u_Ks * vec3(1.0, 1.0, 1.0) * specular, a_Color.a);\n' +
+    '  if (u_Clicked) {\n' + //  Draw in the selected color if mouse is pressed
+    '    v_Color = a_Color_2;\n' +
+    '  }\n' +
     '}\n';
 
 // Fragment shader program
@@ -45,9 +51,11 @@ var Txl = [], Tyl = []; // (x, y) coordinates of a left click
 var Txr = [], Tyr = []; // (x, y) coordinates of a right click
 var countl = 0; // # of red trees
 var countr = 0; // # of blue trees
+var map = []; // each element contains a point, its count, a tag for red or blue and its xy coordinates
+var count = 0; // # of total trees
 // toggle1: 0 for top view, 1 for side view
 // toggle2: 0 for ortho view, 1 for pers view
-// toggle3: 0 for solid, 1 for wireframe
+// toggle3: 0 for flat, 1 for smooth, 2 for wireframe
 var toggle1 = 0, toggle2 = 0, toggle3 = 0;
 function main() {
     // Retrieve <canvas> element
@@ -73,18 +81,20 @@ function main() {
     // Get locations of all the variables for drawing a cylinder
     cylinderProgram.a_Position = gl.getAttribLocation(cylinderProgram, 'a_Position');
     cylinderProgram.a_Color = gl.getAttribLocation(cylinderProgram, 'a_Color');
+    cylinderProgram.a_Color_2 = gl.getAttribLocation(cylinderProgram, 'a_Color_2');
     cylinderProgram.a_Normal = gl.getAttribLocation(cylinderProgram, 'a_Normal');
     cylinderProgram.u_mvpMatrix = gl.getUniformLocation(cylinderProgram, 'u_mvpMatrix');
     cylinderProgram.u_Translation = gl.getUniformLocation(cylinderProgram, 'u_Translation');
     cylinderProgram.u_LightColor = gl.getUniformLocation(cylinderProgram, 'u_LightColor');
     cylinderProgram.u_LightDirection = gl.getUniformLocation(cylinderProgram, 'u_LightDirection');
     cylinderProgram.u_shininessVal = gl.getUniformLocation(cylinderProgram, 'u_shininessVal');
-    cylinderProgram.u_modelview = gl.getUniformLocation(cylinderProgram, 'u_modelview');
+    cylinderProgram.u_Clicked = gl.getUniformLocation(cylinderProgram, 'u_Clicked');
+    cylinderProgram.u_Ks = gl.getUniformLocation(cylinderProgram, 'u_Ks');
 
-    if (cylinderProgram.a_Position < 0 || cylinderProgram.a_Color < 0 || cylinderProgram.a_Normal < 0 ||
-        cylinderProgram.u_ViewMatrix < 0 || cylinderProgram.u_ProjMatrix < 0 || cylinderProgram.u_Translation < 0 ||
-        cylinderProgram.u_LightColor < 0 || cylinderProgram.u_LightDirection < 0 || cylinderProgram.u_shininessVal < 0 ||
-        cylinderProgram.u_modelview < 0) {
+    if (cylinderProgram.a_Position < 0 || cylinderProgram.a_Color < 0 || cylinderProgram.a_Color_2 < 0 ||
+        cylinderProgram.a_Normal < 0 || cylinderProgram.u_ViewMatrix < 0 || cylinderProgram.u_ProjMatrix < 0 ||
+        cylinderProgram.u_Translation < 0 || cylinderProgram.u_LightColor < 0 || cylinderProgram.u_LightDirection < 0 ||
+        cylinderProgram.u_shininessVal < 0 || cylinderProgram.u_Clicked < 0 || cylinderProgram.u_Ks < 0) {
         console.log('Failed to locate variables for cylinder');
         return -1;
     }
@@ -96,7 +106,14 @@ function main() {
     // Clear color and depth buffer
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    canvas.onmousedown = function (ev) { click(ev, gl, canvas, cylinderProgram) };
+    canvas.onmousedown = function (ev) {
+        if (ev.shiftKey == 1) {
+            redraw(ev, gl, cylinderProgram);
+        }
+        else {
+            click(ev, gl, canvas, cylinderProgram);
+        }
+    };
 
     var checkbox1 = document.getElementById('toggle1');
     checkbox1.addEventListener('change', function () {
@@ -119,8 +136,7 @@ function main() {
             setView(gl, cylinderProgram);
         }
     });
-    
-    toggle3 = 0;
+
     setView(gl, cylinderProgram);
 
     var rad = document.form.tree;
@@ -158,6 +174,127 @@ function main() {
         }
     });
 }
+
+/* redraw
+ * input:
+ * ev, gl, cylinderProgram
+ * output:
+ * none
+ * use:
+ * redraw the scene after a shift+click
+ */
+function redraw(ev, gl, cylinderProgram) {
+    var x = ev.clientX, y = ev.clientY;
+    var rect = ev.target.getBoundingClientRect();
+    x = x - rect.left;
+    y = rect.bottom - y;
+    var tree = []; // store the clicked tree's left/right and xy coordinates
+    for (var i = 0; i < count; i++) {
+        var newmap = map[i]; // newmap[0]: color, newmap[1]: left/right, newmap[2]: x coord, newmap[3]: y coord
+        var color = newmap[0] / 255.0;
+        gl.useProgram(cylinderProgram);
+        initPositions(gl, cylinderProgram, newmap[1]);
+        initColors(gl, cylinderProgram, newmap[1]);
+        initLightColor(gl, cylinderProgram);
+        initLightDirection(gl, cylinderProgram);
+        initMatrix(gl, cylinderProgram, toggle1, toggle2);
+        initTranslation(gl, cylinderProgram, newmap[2], newmap[3], toggle2);
+        initGloss(gl, cylinderProgram, newmap[1]);
+        initNormals(gl, cylinderProgram, newmap[1], 0);
+        gl.uniform1i(cylinderProgram.u_Clicked, 1);
+        gl.vertexAttrib4f(cylinderProgram.a_Color_2, color, 0.0, 0.0, 1.0);
+        gl.uniform3fv(cylinderProgram.u_Ks, [1.0, 1.0, 1.0]);
+        var len;
+        if (newmap[1] == 0) {
+            len = cylinderl.length / 3;
+        }
+        else {
+            len = cylinderr.length / 3;
+        }
+        gl.drawArrays(gl.TRIANGLES, 0, len);
+    }
+
+    var pixels = new Uint8Array(4); // Array for storing the pixel value
+    gl.readPixels(x, y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+    for (var i = 0; i < count; i++) {
+        var newmap = map[i];
+        if (pixels[0] == newmap[0] && pixels[0] + pixels[1] + pixels[2] + pixels[3]!= 0) { // get the clicked tree's info
+            tree.push(newmap[1]);
+            tree.push(newmap[2]);
+            tree.push(newmap[3]);
+            break;
+        }
+    }
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    for (var i = 0; i < count; i++) {
+        var newmap = map[i];
+        if (tree.length == 0 || newmap[2] != tree[1] || newmap[3] != tree[2]) {
+            gl.useProgram(cylinderProgram);
+            gl.uniform1i(cylinderProgram.u_Clicked, 0);
+            initPositions(gl, cylinderProgram, newmap[1]);
+            initColors(gl, cylinderProgram, newmap[1]);
+            initLightColor(gl, cylinderProgram);
+            initLightDirection(gl, cylinderProgram);
+            initMatrix(gl, cylinderProgram, toggle1, toggle2);
+            initTranslation(gl, cylinderProgram, newmap[2], newmap[3], toggle2);
+            initGloss(gl, cylinderProgram, newmap[1]);
+            gl.uniform3fv(cylinderProgram.u_Ks, [1.0, 1.0, 1.0]);
+            var len;
+            if (newmap[1] == 0) {
+                len = cylinderl.length / 3;
+            }
+            else {
+                len = cylinderr.length / 3;
+            }
+            if (toggle3 == 0) { // Flat shading
+                initNormals(gl, cylinderProgram, newmap[1], 0);
+                gl.drawArrays(gl.TRIANGLES, 0, len);
+            }
+            else if (toggle3 == 1) { // smooth
+                initNormals(gl, cylinderProgram, newmap[1], 1);
+                gl.drawArrays(gl.TRIANGLES, 0, len);
+            }
+            else if (toggle3 == 2) { // wireframe using flat shading normals
+                initNormals(gl, cylinderProgram, newmap[1], 0);
+                gl.drawArrays(gl.LINES, 0, len);
+            }
+        }
+    }
+    if (tree.length != 0) {
+        gl.useProgram(cylinderProgram);
+        gl.uniform1i(cylinderProgram.u_Clicked, 0);
+        initPositions(gl, cylinderProgram, tree[0]);
+        gl.vertexAttrib4f(cylinderProgram.a_Color, 0.0, 1.0, 1.0, 1.0);
+        initLightColor(gl, cylinderProgram);
+        initLightDirection(gl, cylinderProgram);
+        initMatrix(gl, cylinderProgram, toggle1, toggle2);
+        initTranslation(gl, cylinderProgram, tree[1], tree[2], toggle2);
+        gl.uniform1f(cylinderProgram.u_shininessVal, 5.0);
+        gl.uniform3fv(cylinderProgram.u_Ks, [0.0, 1.0, 0.0]);
+        var len;
+        if (tree[0] == 0) {
+            len = cylinderl.length / 3;
+        }
+        else {
+            len = cylinderr.length / 3;
+        }
+        if (toggle3 == 0) { // Flat shading
+            initNormals(gl, cylinderProgram, tree[0], 0);
+            gl.drawArrays(gl.TRIANGLES, 0, len);
+        }
+        else if (toggle3 == 1) { // smooth
+            initNormals(gl, cylinderProgram, tree[0], 1);
+            gl.drawArrays(gl.TRIANGLES, 0, len);
+        }
+        else if (toggle3 == 2) { // wireframe using flat shading normals
+            initNormals(gl, cylinderProgram, tree[0], 0);
+            gl.drawArrays(gl.LINES, 0, len);
+        }
+    }
+}
+
 
 /* save: refer to https://jsfiddle.net/4v26ebtp/
  * 
@@ -234,7 +371,7 @@ function load() {
             Txr.push(txr[i]);
             Tyr.push(tyr[i]);
         }
-    };
+    }
 }
 
 /* setView
@@ -252,6 +389,8 @@ function setView(gl, cylinderProgram) {
     // Clear color and depth buffer
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     for (var i = 0; i < countl; i++) { // Draw red trees
+        gl.useProgram(cylinderProgram);
+        gl.uniform1i(cylinderProgram.u_Clicked, 0);
         initPositions(gl, cylinderProgram, 0);
         initColors(gl, cylinderProgram, 0);
         initLightColor(gl, cylinderProgram);
@@ -259,6 +398,7 @@ function setView(gl, cylinderProgram) {
         initMatrix(gl, cylinderProgram, toggle1, toggle2);
         initTranslation(gl, cylinderProgram, Txl[i], Tyl[i], toggle2);
         initGloss(gl, cylinderProgram, 0);
+        gl.uniform3fv(cylinderProgram.u_Ks, [1.0, 1.0, 1.0]);
         var len = cylinderl.length / 3;
         if (toggle3 == 0) { // Flat shading
             initNormals(gl, cylinderProgram, 0, 0);
@@ -274,6 +414,8 @@ function setView(gl, cylinderProgram) {
         }
     }
     for (var i = 0; i < countr; i++) { // Draw blue trees
+        gl.useProgram(cylinderProgram);
+        gl.uniform1i(cylinderProgram.u_Clicked, 0);
         initPositions(gl, cylinderProgram, 1);
         initColors(gl, cylinderProgram, 1);
         initLightColor(gl, cylinderProgram);
@@ -281,6 +423,7 @@ function setView(gl, cylinderProgram) {
         initMatrix(gl, cylinderProgram, toggle1, toggle2);
         initTranslation(gl, cylinderProgram, Txr[i], Tyr[i], toggle2);
         initGloss(gl, cylinderProgram, 1);
+        gl.uniform3fv(cylinderProgram.u_Ks, [1.0, 1.0, 1.0]);
         var len = cylinderr.length / 3;
         if (toggle3 == 0) { // Flat shading
             initNormals(gl, cylinderProgram, 1, 0);
@@ -306,10 +449,22 @@ function click(ev, gl, canvas, cylinderProgram) {
     y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
 
     if (ev.button === 0) {
+        var newmap = [];
+        newmap.push((26 * count++) % 255); // color.r
+        newmap.push(0); // left click
+        newmap.push(x);
+        newmap.push(y);
+        map.push(newmap);
         countl++;
         Txl.push(x); Tyl.push(y);
     }
     else if (ev.button === 2) {
+        var newmap = [];
+        newmap.push((26 * count++) % 255); // color.r
+        newmap.push(1); // right click
+        newmap.push(x);
+        newmap.push(y);
+        map.push(newmap);
         countr++;
         Txr.push(x); Tyr.push(y);
     }
@@ -400,7 +555,6 @@ function initNormals(gl, cylinderProgram, tag1, tag2) {
             // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
             gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-            //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
             gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
             // Turn on the normal attribute
             gl.enableVertexAttribArray(cylinderProgram.a_Normal);
@@ -416,7 +570,6 @@ function initNormals(gl, cylinderProgram, tag1, tag2) {
             // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
             gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-            //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
             gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
             // Turn on the normal attribute
             gl.enableVertexAttribArray(cylinderProgram.a_Normal);
@@ -434,7 +587,6 @@ function initNormals(gl, cylinderProgram, tag1, tag2) {
             // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
             gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-            //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
             gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
             // Turn on the normal attribute
             gl.enableVertexAttribArray(cylinderProgram.a_Normal);
@@ -450,7 +602,6 @@ function initNormals(gl, cylinderProgram, tag1, tag2) {
             // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = normalBuffer)
             gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
             gl.bufferData(gl.ARRAY_BUFFER, normals, gl.STATIC_DRAW);
-            //var normalLocation = gl.getAttribLocation(gl.program, "a_Normal");
             gl.vertexAttribPointer(cylinderProgram.a_Normal, 3, gl.FLOAT, false, 0, 0);
             // Turn on the normal attribute
             gl.enableVertexAttribArray(cylinderProgram.a_Normal);
@@ -554,6 +705,11 @@ function initTranslation(gl, cylinderProgram, tx, ty, tag) {
     }
 }
 
+/* initGloss
+ * input:
+ * gl, cylinderProgram
+ * tag: 0 for red trees, 1 for blue trees
+ */
 function initGloss(gl, cylinderProgram, tag) {
     gl.useProgram(cylinderProgram);
     if (tag == 0) {
