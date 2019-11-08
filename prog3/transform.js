2f164c2d64fd087a082d682e1fe71f33fbf3983e
QiaowenYoung <17307130294@fudan.edu.cn>
2019-11-07 17:51:48 -0800

    11/7/19

--- prog3/transform.js
+++ prog3/transform.js
@@ -285,9 +285,7 @@ function main() {
             var rect = ev.target.getBoundingClientRect();
 
             y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
-            console.log(1);
-            console.log(y-offz);
-            selected[4] += y - offz;
+            selected[4] += (y - offz) / 200;
             offz = y;
             isUp = 0;
             draw(gl, cylinderProgram);
